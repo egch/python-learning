06c0024443dd82fa37d6e114b5cdcc81cd07b5d5
Enrico Giurin <enricogiurin@gmail.com>
2025-11-28 14:57:21 +0100

    if else

--- src/udemy/fastapi/lecture1/BooleanAndOperators.py
+++ src/udemy/fastapi/lecture1/BooleanAndOperators.py
@@ -15,3 +15,26 @@ print(1 > 2)  # False
 print(1 > 3 and 5 < 7)  # False
 print(1 > 3 or 5 < 7)  # True
 print(not(1 == 1))  # False
+
+"""
+If
+"""
+
+x = 2
+if x == 1:
+    print("x is 1")
+else:
+    print("x is not 1")
+
+
+print("outside")
+
+
+hour = 20
+
+if hour < 15:
+    print("good morning")
+elif hour < 20:
+    print("good afternoon")
+else:
+    print("good nite")
