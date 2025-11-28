c4e080fd4a2deea1145f146fcaab953aa25a0230
Enrico Giurin <enricogiurin@gmail.com>
2025-11-28 02:44:02 +0100

    work in progress List assignment

---
+++ src/udemy/fastapi/lecture1/Lists_Assignment.py
@@ -0,0 +1,18 @@
+"""
+- Create a list of 5 animals called zoo
+
+- Delete the animal at the 3rd index.
+
+- Append a new animal at the end of the list
+
+- Delete the animal at the beginning of the list.
+
+- Print all the animals
+
+- Print only the first 3 animals
+"""
+zoo = ['tiger', 'lion', 'cat', 'dog', 'monkey']
+zoo.remove(3)
+zoo.append('pinguin')
+print(zoo)
+print(zoo[0: ])
