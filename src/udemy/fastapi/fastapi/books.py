1fe72768de8d440b1f8d13ac90b607ad62115584
Enrico Giurin <enricogiurin@gmail.com>
2025-12-15 11:46:14 +0100

    Path Parameters

--- src/udemy/fastapi/fastapi/books.py
+++ src/udemy/fastapi/fastapi/books.py
@@ -12,6 +12,8 @@ BOOKS =[
 
 ]
 
+
+# The order of api top-down matters!
 @app.get("/")
 async def first_api():
 #def first_api():
@@ -21,4 +23,24 @@ async def first_api():
 @app.get("/books")
 async def read_all_books():
     return  BOOKS
+
+#########################################
+######### Path Parameters ##############
+#########################################
+@app.get("/books/title/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+# it's important to specify the type in this case
+
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param: int):
+    return BOOKS[dynamic_param]
+
+#########################################
+######### Query Parameters ##############
+#########################################
+
 
