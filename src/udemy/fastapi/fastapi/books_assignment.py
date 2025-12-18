bd7ccb4cf1fe014595f5da66cdade48f71cf187d
Enrico Giurin <enricogiurin@gmail.com>
2025-12-18 18:56:29 +0100

    DELETE verb & assignment - unneeded import Body

--- src/udemy/fastapi/fastapi/books_assignment.py
+++ src/udemy/fastapi/fastapi/books_assignment.py
@@ -3,7 +3,7 @@
 # 1. Create a new API Endpoint that can fetch all books from a specific author using either Path Parameters
 # or Query Parameters.
 #
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 
 
 app = FastAPI()
