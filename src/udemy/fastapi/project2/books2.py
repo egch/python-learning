4678850a64d6f996e6736c147b8b40730e6a962c
Enrico Giurin <enricogiurin@gmail.com>
2025-12-23 12:56:29 +0100

    implemented PUT & DELETE

--- src/udemy/fastapi/project2/books2.py
+++ src/udemy/fastapi/project2/books2.py
@@ -63,6 +63,12 @@ BOOKS = [
 ]
 
 
+# we want to increase the id
+# BOOKS[-1] -> return the last element of the collection
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
@@ -78,7 +84,15 @@ async def read_book(book_id: int):
 @app.get("/books/")
 async def read_book_by_rating(book_rating: int):
     books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
 
+
+##########################################
+###### PUT/POST/DELETE ##########################
+##########################################
 
 @app.post("/create_book")
 async def create_book(book_request: BookRequest):
@@ -86,9 +100,18 @@ async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
 
+@app.put("/update_book")
+async def update_book(book: BookRequest):
+    for index in range(len(BOOKS)):
+        if BOOKS[index].id == book.id:
+            BOOKS[index] = book
 
-# we want to increase the id
-# BOOKS[-1] -> return the last element of the collection
-def find_book_id(book: Book):
-    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
-    return book
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for index in range(len(BOOKS)):
+        if BOOKS[index].id == book_id:
+            BOOKS.pop(index)
+            break
+
+
+
