f9e9d84fa543cf594bf076ad67ecfb11736db469
Enrico Giurin <enricogiurin@gmail.com>
2026-01-03 15:55:59 +0100

    starting FK

--- src/udemy/fastapi/project3/todoapp/routers/auth.py
+++ src/udemy/fastapi/project3/todoapp/routers/auth.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from models import Users
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+class CreateUserRequest(BaseModel):
+
+    email: str
+    username:  str
+    firstName: str
+    lastName: str
+    password: str
+    role: str
+
+
+
+@router.post("/auth/")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username= create_user_request.username,
+        firstName=create_user_request.firstName,
+        lastName=create_user_request.lastName,
+        role= create_user_request.role,
+        hashed_password= create_user_request.password,
+        is_active = True
+    )
+    return create_user_model
---
+++ src/udemy/fastapi/project3/todoapp/todos.md
@@ -0,0 +1,9 @@
+# Todos 
+You need to have a pycharm project starting from this folder!
+
+## Commands
+```shell
+ python -m pip install --upgrade pip
+ pip install passlib
+pip install bcrypt==4.0.1
+```
