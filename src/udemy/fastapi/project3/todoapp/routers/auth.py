662b0f78a94261f6ee54e49e66752367368fb6ab
Enrico Giurin <enricogiurin@gmail.com>
2026-01-07 12:53:48 +0100

    login

--- src/udemy/fastapi/project3/todoapp/routers/auth.py
+++ src/udemy/fastapi/project3/todoapp/routers/auth.py
@@ -63,4 +63,7 @@ async def create_user(db: db_dependency,
 
 @router.post("/token")
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    return form_data.username
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed authentication'
+    return 'Successful authentication'
