e9ba0486a9030b5caef34ca982772517d850ef9f
Enrico Giurin <enricogiurin@gmail.com>
2026-04-13 22:11:56 +0200

    api for update phone_number

--- apps/todoapp/src/routers/users.py
+++ apps/todoapp/src/routers/users.py
@@ -36,6 +36,7 @@ class UserVerification(BaseModel):
     new_password: str = Field(min_length=6)
 
 
+
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependency, db:db_dependency):
     if user is None:
@@ -53,6 +54,18 @@ async def change_password(user: user_dependency, db: db_dependency, user_verific
     if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
         raise HTTPException(status_code=401, detail='Error on password change')
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+
+
+@router.put('/phonenumber/{phone_number}', status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(user: user_dependency, db: db_dependency, phone_number: str):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    print('user found')
+
+    user_model.phone_number = phone_number
     db.add(user_model)
     db.commit()
 
