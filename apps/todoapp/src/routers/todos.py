06781914cf42c51f17140bf490f2cbdb4dc5eac9
Enrico Giurin <enricogiurin@gmail.com>
2026-05-23 01:51:57 +0200

    completed add-todo page

--- apps/todoapp/src/routers/todos.py
+++ apps/todoapp/src/routers/todos.py
@@ -60,6 +60,16 @@ async def render_todo_page(request: Request, db: db_dependency):
         return redirect_to_login()
 
 
+@router.get("/add-todo-page")
+async def render_add_todo_page(request: Request):
+    try:
+        user = await get_current_user(request.cookies.get('access_token'))
+        if user is None:
+            return  redirect_to_login()
+        return templates.TemplateResponse("add-todo.html", {"request": request, "user": user })
+    except Exception:
+        return redirect_to_login()
+
 
 
 ### Endpoints ###
