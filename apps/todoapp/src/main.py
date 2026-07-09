52926885f32b125c9a38ffb2d5b8bfe12cedd4d1
Enrico Giurin <enricogiurin@gmail.com>
2026-07-09 21:01:47 +0200

    implemented redirect

--- apps/todoapp/src/main.py
+++ apps/todoapp/src/main.py
@@ -1,13 +1,14 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 
 from . import models
 from .database import engine
 from .routers import auth, todos, admin, users
-from fastapi.templating import Jinja2Templates
+
 
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,13 +16,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-templates = Jinja2Templates(directory="templates")
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
 
 @app.get("/healthy")
 def health_check():
